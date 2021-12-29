bdc1d99ad44c41e18b8c9601f7f2116860a8abd2
Krutonium <PFCKrutonium@gmail.com>
2021-12-29 07:43:04 -0500

    Forgot dotnet

--- devices/gaming-pc.nix
+++ devices/gaming-pc.nix
@@ -62,6 +62,11 @@ in
           pkgs.linuxPackages.zenpower
           pkgs.home-manager
           pkgs.zfs
+          (with pkgs.dotnetCorePackages; combinePackages [
+              sdk_3_1
+              sdk_5_0
+              sdk_6_0
+          ])
     ];
 
 
