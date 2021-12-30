a29affd47a05a4930b09ad8194cc29f1d9cca44e
Krutonium <PFCKrutonium@gmail.com>
2021-12-30 02:32:19 -0500

    Add xfce

---
+++ desktops/xfce.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+{
+    services.xserver.enable = true;
+    hardware.nvidia.modesetting.enable = false;
+    programs.xwayland.enable = true;
+    hardware.opengl.enable = true;
+    services.xserver.displayManager.lightdm.enable = true;
+    services.xserver.desktopManager.xfce.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+    };
+}
