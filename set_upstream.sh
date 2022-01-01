16518ce7df9e0206c7a222ab1e2f9d9aeecd5bd1
Krutonium <PFCKrutonium@gmail.com>
2022-01-01 04:35:48 -0500

    Add script to set up pushes

---
+++ set_upstream.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#git remote add github git@github.com:Krutonium/My_Unified_NixOS_Config.git
+#git remote add gitea gitea@gitea.krutonium.ca:Krutonium/My_Unified_NixOS_Config.git
+git remote set-url --add --push origin git@github.com:Krutonium/My_Unified_NixOS_Config.git
+git remote set-url --add --push origin gitea@gitea.krutonium.ca:Krutonium/My_Unified_NixOS_Config.git
+
