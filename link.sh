d5811b7b8b698dd89aebad86ab32566a774b234b
Krutonium <PFCKrutonium@gmail.com>
2022-01-07 18:18:47 -0500

    Cleanup

--- link.sh
+++ link.sh
@@ -2,5 +2,3 @@
 sudo mv /etc/nixos/ /etc/nixos.bak/ || true
 sudo ln -s $(pwd) /etc/nixos
 sudo nixos-generate-config
-sudo rm /auth
-sudo ln -s $(pwd)/auth /auth
--- relink.sh
+++ relink.sh
@@ -2,5 +2,4 @@
 sudo rm /etc/nixos
 sudo ln -s $(pwd) /etc/nixos
 sudo nixos-generate-config
-sudo rm /auth
-sudo ln -s $(pwd)/auth /auth
+
