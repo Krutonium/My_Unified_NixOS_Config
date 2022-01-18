407957c5060f0b8e234276f727451071071ec0bf
Krutonium <PFCKrutonium@gmail.com>
2022-01-18 12:52:16 -0500

    Script Improvements

--- link.sh
+++ link.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-sudo mv /etc/nixos/ /etc/nixos.bak/ || true
-sudo ln -s $(pwd) /etc/nixos
-sudo nixos-generate-config
+
+# This script should restart as root if it's not already
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exec sudo "$0" "$@"
+fi
+
+mv /etc/nixos/ /etc/nixos.bak/ || true
+ln -s $(pwd) /etc/nixos
+nixos-generate-config
--- relink.sh
+++ relink.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-sudo rm /etc/nixos
-sudo ln -s $(pwd) /etc/nixos
-sudo nixos-generate-config
+
+# This script should restart as root if it's not already
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exec sudo "$0" "$@"
+fi
+
+rm /etc/nixos
+ln -s $(pwd) /etc/nixos
+nixos-generate-config
 
