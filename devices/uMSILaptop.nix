afcb5bfba4df38e875d2c20cd1029b2ab058273b
Krutonium <PFCKrutonium@gmail.com>
2022-01-18 14:30:23 -0500

    Waydroid :D

--- devices/uMSILaptop.nix
+++ devices/uMSILaptop.nix
@@ -17,8 +17,9 @@ in
         ../packages/dotnet.nix
         ../packages/home-manager.nix
         ../packages/steam.nix
-	    ../packages/wine.nix
-	    ../packages/ccache.nix
+	../packages/wine.nix
+	../packages/ccache.nix
+	../packages/waydroid.nix
         <nixos-hardware/common/pc>
         <nixos-hardware/common/pc/ssd>
         <nixos-hardware/common/cpu/intel>
