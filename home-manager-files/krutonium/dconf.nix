59a75a98789f367a80415e62c9dbacbc4e5b2a0d
Krutonium <PFCKrutonium@gmail.com>
2022-02-01 13:37:54 -0500

    hm/dconf: fix wallpaper path

--- home-manager-files/krutonium/dconf.nix
+++ home-manager-files/krutonium/dconf.nix
@@ -227,7 +227,7 @@ in
       color-shading-type = "solid";
       picture-opacity = 100;
       picture-options = "zoom";
-      picture-uri = "file://${./background.png}";
+      picture-uri = "file://${./wallpaper.png}";
     };
   };
 }
