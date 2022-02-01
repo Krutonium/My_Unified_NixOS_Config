de6c2a24491d1f528e59556c7e43b8f336124fbe
Krutonium <PFCKrutonium@gmail.com>
2022-02-01 11:18:32 -0500

    SSH for notkea

--- services/fail2ban.nix
+++ services/fail2ban.nix
@@ -2,6 +2,6 @@
 {
     services.fail2ban = {
 	enable = true;
-	ignoreIP = [ "192.168.0.0/16" ];
+	ignoreIP = [ "192.168.0.0/16" "83.114.116.175" ];
     };
 }
--- services/ssh.nix
+++ services/ssh.nix
@@ -1,9 +1,10 @@
 { config, ...}:
 {
-    networking.firewall.allowedTCPPorts = [ 22 ];
+    networking.firewall.allowedTCPPorts = [ 22 1337 ];
     services.openssh = {
         enable = true;
         permitRootLogin = "without-password";
         passwordAuthentication = false;
+	ports = [ 22 1337 ];
     };
 }
