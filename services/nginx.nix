8353b5189f18c1fadd29ad5cd7ca29d8cefc6d58
Krutonium <PFCKrutonium@gmail.com>
2022-02-08 14:23:49 -0500

    Attempt to make Matrix work

--- services/nginx.nix
+++ services/nginx.nix
@@ -41,6 +41,9 @@
           add_header Access-Control-Allow-Origin *;
           return 200 '${builtins.toJSON client}';
         '';
+        locations."/_matrix" = {
+          proxyPass = "http://127.0.0.1:8008"; # without a trailing /
+        };
     };
     "dynmap.krutonium.ca" = {
       forceSSL = true;
@@ -124,7 +127,7 @@
     "synapse.krutonium.ca" = {
       forceSSL = true;
       enableACME = true;
-      locations."/".proxyPass = "http://127.0.0.1:8008/";
+      locations."/".proxyPass = "http://127.0.0.1:8008";
     };
   };
 }
