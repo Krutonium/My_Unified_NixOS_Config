e3f3805ea3dea50ae1dc2e94d594bc22027e9e64
Krutonium <PFCKrutonium@gmail.com>
2022-02-11 15:07:54 -0500

    PO

---
+++ services/project_ozone_3.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+let
+  workingdir = "/srv/StoneBlock";
+  user = "gameserver";
+in
+{
+  networking.firewall.allowedTCPPorts = [ 25565 ];
+  systemd.services.stoneblock = {
+    description = "Stoneblock Game Server";
+    serviceConfig = {
+      Type = "simple";
+      WorkingDirectory = workingdir;
+      User = user;
+      Restart = "on-failure";
+      KillSignal = "SIGINT";
+    };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "networking.target" ];
+    path = [ pkgs.openjdk8 pkgs.steam-run-native pkgs.screen ];
+    script = ''
+      steam-run screen -DmS ozone ${workingdir}/LaunchServer.sh
+    '';
+    enable = true;
+  };
+}
