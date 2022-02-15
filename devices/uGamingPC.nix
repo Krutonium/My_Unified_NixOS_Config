8adf56bc76bc9f4f5f2d7fff560c4a1d03a4e35c
Krutonium <PFCKrutonium@gmail.com>
2022-02-15 01:17:56 -0500

    Ugh

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -3,7 +3,7 @@
 
 #Run sudo nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
 let
-  kernel = pkgs.linuxPackages_latest;
+  kernel = pkgs.linuxPackages_zen;
   Hostname = "uGamingPC";
 in
 {
