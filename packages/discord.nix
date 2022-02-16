11046b71a9f2ccc866eed273db049f7f4c578d68
Krutonium <PFCKrutonium@gmail.com>
2022-02-16 03:53:54 -0500

    Discord in a Wrapper

--- packages/discord.nix
+++ packages/discord.nix
@@ -1,15 +1,23 @@
 # Specifically to prevent Discord from accessing my Webcams, causing my screen to flash.
 {pkgs, config, lib, ...}:
+let
+    discordScript = pkgs.writeScriptBin "discordWrapper"
+      ''
+        #!${pkgs.bash}/bin/bash
+        firejail discord
+      '';
+in
 {
     environment.systemPackages = [
         pkgs.discord
         pkgs.firejail
+        discordScript
     ];
+    programs.firejail.enable = true;
     programs.firejail.wrappedBinaries = {
       discord = {
-
-          executable = "${lib.getBin pkgs.discord}/bin/discord";
-          profile = "${pkgs.firejail}/etc/firejail/discord.profile";
+          executable = "${pkgs.discord}/bin/discord";
+          profile = ./discord.profile;
       };
     };
 }
---
+++ packages/discord.profile
@@ -0,0 +1,1 @@
+blacklist /var/video*
