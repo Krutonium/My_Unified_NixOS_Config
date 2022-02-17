00df82b2bbe02702caa007ef50012b8c8d65d876
Krutonium <PFCKrutonium@gmail.com>
2022-02-16 23:00:45 -0500

    Update

--- packages/scripts.nix
+++ packages/scripts.nix
@@ -1,15 +1,20 @@
 { config, pkgs, ... }:
 let
   update = pkgs.writeShellScriptBin "update" ''
+    echo Updating Local System
+    updatePackages
     cd /etc/nixos/
-    git pull
+    colmena apply switch --apply-local
+  '';
+  updatePackages = pkgs.writeShellScriptBin "updateConfig"''
+    echo Updating Commit File...
+    cd /etc/nixos/
     nix flake update --commit-lock-file
-    colmena apply switch --no-substitutes --on $(hostname)
   '';
   updateAll = pkgs.writeShellScriptBin "updateAll" ''
+    echo Updating All Systems
+    updatePackages
     cd /etc/nixos/
-    git pull
-    nix flake update --commit-lock-file
     colmena apply switch --no-substitutes
   '';
   resetConfig = pkgs.writeShellScriptBin "resetConfig" ''
@@ -27,21 +32,7 @@ let
     git commit -m "Update"
     git push
   '';
-  rebootEverything = pkgs.writeShellScriptBin "rebootEverything" ''
-    ssh root@uwebserver 'shutdown -r 1'
-    ssh root@umsilaptop 'shutdown -r 1'
-    ssh root@uhplaptop 'shutdown -r 1'
-    ssh root@ugamingpc 'shutdown -r 1'
-    echo "Done"
-  '';
-  rebootCancel = pkgs.writeShellScriptBin "rebootCancel" ''
-    ssh root@uwebserver 'shutdown -c'
-    ssh root@umsilaptop 'shutdown -c'
-    ssh root@uhplaptop 'shutdown -c'
-    ssh root@ugamingpc 'shutdown -c'
-    echo "Done"
-  '';
 in
 {
-  environment.systemPackages = [ update updateAll resetConfig pushConfig rebootEverything rebootCancel ];
+  environment.systemPackages = [ update updatePackages updateAll resetConfig pushConfig ];
 }
