2cbb09fb4dff915ae9dc90e69370f1369357a629
Krutonium <PFCKrutonium@gmail.com>
2022-02-19 12:52:49 -0500

    Dynamically Create the service.

--- services/fancontroller.nix
+++ services/fancontroller.nix
@@ -3,5 +3,18 @@
   environment.systemPackages = [
     pkgs.BetterFanController
   ];
-  systemd.services.betterfancontroller.enable = true;
+  systemd.services.betterfancontroller = {
+    description = "Better Fan Controller";
+    serviceConfig = {
+      Type = "simple";
+      WorkingDirectory = "/tmp/";
+      User = "root";
+      Restart = "always";
+    };
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      /run/current-system/sw/bin/BetterFanController
+    '';
+    enable = true;
+  };
 }
