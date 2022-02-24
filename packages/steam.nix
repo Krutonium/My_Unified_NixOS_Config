7648d4d52a6ec9fe54c01224e0e780624e5bdcb9
Krutonium <PFCKrutonium@gmail.com>
2022-02-23 21:04:51 -0500

    Update

--- packages/steam.nix
+++ packages/steam.nix
@@ -1,6 +1,21 @@
 { config, pkgs, ... }:
+let
+  steamScript = pkgs.writeShellScriptBin "steam"
+    ''
+      mangohud --dlsym ${pkgs.steam}/bin/steam
+    '';
+  lowSteam = pkgs.steam.overrideAttrs (oldAttrs: { meta.priority = 10; });
+
+  #What have we learned today? Basically, this doesn't work. It ends up making Steam either not start at all, or start with a blank screen.
+in
 {
+
   # Gotta have good old steam
   programs.steam.enable = true;
   hardware.steam-hardware.enable = true;
+  environment.systemPackages = [
+    #steamScript
+    lowSteam
+  ];
+
 }
