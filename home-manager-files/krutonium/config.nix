d8031e4e970bf8e333233362275b2e434956ff6c
Krutonium <PFCKrutonium@gmail.com>
2022-02-25 20:15:47 -0500

    Discord Config in Config

--- home-manager-files/krutonium/config.nix
+++ home-manager-files/krutonium/config.nix
@@ -8,5 +8,19 @@
       set softwrap
       set tabstospaces
     '';
+    ".config/discord/settings.json".text = ''
+      {
+        "BACKGROUND_COLOR": "#202225",
+        "IS_MAXIMIZED": false,
+        "IS_MINIMIZED": false,
+        "SKIP_HOST_UPDATE: false",
+        "WINDOW_BOUNDS": {
+          "x": 307,
+          "y" = 127,
+          "width" = 1280,
+          "height" = 725
+        }
+      }
+    '';
   };
 }
