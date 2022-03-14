597f381767f33b1d5e699a3addd077332b35577d
Krutonium <PFCKrutonium@gmail.com>
2022-03-13 20:37:11 -0400

    Typo

--- packages/candy-icons.nix
+++ packages/candy-icons.nix
@@ -1,6 +1,6 @@
 {config, pkgs, fetchurl, ...}:
 let
-  icon_file = builins.fetchurl {
+  icon_file = builtins.fetchurl {
     url = "https://github.com/EliverLara/candy-icons/archive/6503d27bd95d0ad5c1f57ea7a709fe9dd65af547.zip";
     sha256 = "0bal0vz2qr7wsr3m0v2qh1f3hjb7fqxnw4h1dxx5r1fbjy6qm09a";
   };
