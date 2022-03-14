f4b8bbde9f4fc9a8294637dc37126b74ded2bbd4
Krutonium <PFCKrutonium@gmail.com>
2022-03-13 21:58:20 -0400

    Adding sha256

--- packages/openrgb.nix
+++ packages/openrgb.nix
@@ -2,6 +2,7 @@
 let
   openrgb-rules = builtins.fetchurl {
     url = "https://gitlab.com/CalcProgrammer1/OpenRGB/-/raw/master/60-openrgb.rules";
+    sha256 = "1pxpal31rzncpg4iznz1pj97sp7i66aiqqyyyfs08z96sl68n28h";
   };
 in
 {
--- packages/plymouth.nix
+++ packages/plymouth.nix
@@ -1,15 +1,20 @@
 { config, pkgs, pkgs-unstable, ... }:
 let
+  aperture_files = builtins.fetchurl {
+    url = "https://github.com/irth/plymouth-theme-aperture/archive/refs/heads/master.zip";
+    sha256 = "09nizxxwbcjjw52sx7yldq14llf9g96n6zqb8ijg4r257rpawmv1";
+  };
+
   aperture-plymouth = pkgs.stdenv.mkDerivation {
     name = "aperture-plymouth";
     buildInputs = [pkgs.unzip];
-    src = builtins.fetchurl https://github.com/irth/plymouth-theme-aperture/archive/refs/heads/master.zip;
+    src = aperture_files;
     buildCommand = ''
       outDir="$out/share/plymouth/themes/"
       mkdir -p $outDir
       unzip $src -d $outDir
       mv $outDir/plymouth-theme-aperture-master $outDir/aperture
-      cat $outDir/aperture/aperture.plymouth | sed  "s@\/usr\/@$out\/@" > $outDir/aperture/aperture.plymouth
+      # cat $outDir/aperture/aperture.plymouth | sed  "s@\/usr\/@$out\/@" > $outDir/aperture/aperture.plymouth
     '';
   };
 in
@@ -19,7 +24,7 @@ in
   ];
 
   boot.plymouth.enable = true;
-  #boot.plymouth.theme = "aperture";
+  boot.plymouth.theme = "aperture";
   boot.plymouth.themePackages = [
     aperture-plymouth
   ];
