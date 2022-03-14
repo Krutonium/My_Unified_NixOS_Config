b113b4132e5966e708b380a0299c354f01a9158a
Krutonium <PFCKrutonium@gmail.com>
2022-03-14 05:24:24 -0400

    d => D

--- packages/plymouth.nix
+++ packages/plymouth.nix
@@ -14,9 +14,8 @@ let
       mkdir -p $outDir
       unzip $src -d $outDir
       mv $outDir/plymouth-theme-aperture-master $outDir/aperture
-      sed "s%/usr/share%$outdir%g" $outDir/aperture/aperture.plymouth | sponge $outDir/aperture/aperture.plymouth
-      cat $outDir/aperture/aperture.plymouth
-
+      sed "s%/usr/share%$outDir%g" $outDir/aperture/aperture.plymouth | sponge $outDir/aperture/aperture.plymouth
+      # Apparently using cat and sed breaks? I don't know why. So we summon an eldritch force to do it for us.
     '';
   };
 in
