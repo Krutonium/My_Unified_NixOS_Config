a465a7f8f9f6a3a20c7b5e0f529f8db3bb9400fc
Krutonium <PFCKrutonium@gmail.com>
2022-03-22 08:05:39 -0400

    VirtualBox

--- packages/virtual_machines.nix
+++ packages/virtual_machines.nix
@@ -1,6 +1,7 @@
 { pkgs, config, ... }:
 {
   virtualisation.libvirtd.enable = true;
+  virtualisation.virtualbox.host.enable = true;
   programs.dconf.enable = true;
   environment.systemPackages = [
     pkgs.virt-manager
