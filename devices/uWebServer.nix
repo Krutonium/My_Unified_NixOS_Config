81c3e3c2e5e2ec4e6ada6c4c60dbe53009d177e9
Krutonium <PFCKrutonium@gmail.com>
2022-04-08 12:19:50 -0400

    Remove Static IP

--- devices/uWebServer.nix
+++ devices/uWebServer.nix
@@ -51,10 +51,6 @@ in
   boot.loader.grub.splashImage = null;
 
   networking.tempAddresses = "disabled";
-  networking.interfaces.enp5s0.ipv4.addresses = [ {
-    address = "192.168.0.10";
-    prefixLength = 24;
-  } ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
   environment.systemPackages = [
