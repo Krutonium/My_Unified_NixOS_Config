3ff274efcec40f624828cd1f63a893b9e20b04ba
Krutonium <PFCKrutonium@gmail.com>
2022-04-12 22:32:35 -0400

    Add USB 3 controller to thing

--- packages/GPU_Passthrough/enter_the_matrix.nix
+++ packages/GPU_Passthrough/enter_the_matrix.nix
@@ -2,7 +2,8 @@
 let
   # I'm going to put all the configurables in here
   # This is the device ID's of my GTX 750 Ti with it's audio device as well.
-  toPassToVM_kernelcmd = [ "vfio-pci.ids=10de:1380,10de:0fbc" ];
+  toPassToVM_kernelcmd = [ "vfio-pci.ids=10de:1380,10de:0fbc,1022:145c" ];
+  # 1022:145c is a random USB 3 controller - Lets find out where!
 in
 {
     #Disable the nouveau driver if the card is nVidia. Not sure how to handle it if there is more than one card, or one is AMD.
