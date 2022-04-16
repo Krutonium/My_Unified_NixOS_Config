6b4bf3b6712ce78cb5f323aa8d6028a99281ef6c
Krutonium <PFCKrutonium@gmail.com>
2022-04-16 04:34:57 -0400

    no nvidia

--- desktops/gnome.nix
+++ desktops/gnome.nix
@@ -4,7 +4,6 @@
   hardware.nvidia.modesetting.enable = false;
   programs.xwayland.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.displayManager.gdm.nvidiaWayland = true;
   hardware.opengl.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
