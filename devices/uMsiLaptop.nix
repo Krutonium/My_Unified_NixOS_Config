d942825d073be1e01c897e8c74d3b20323ee0ea6
Krutonium <PFCKrutonium@gmail.com>
2022-04-17 00:39:15 -0400

    onedrive

--- devices/uMsiLaptop.nix
+++ devices/uMsiLaptop.nix
@@ -29,6 +29,7 @@ in
     device = "/swap";
     size = 1024 * 16; #16GB
   }];
+  services.onedrive.enable = true;
   hardware.bumblebee.enable = true;
   home-manager.users.krutonium = import ../home-manager-files/krutonium/home.nix;
   networking.hostName = Hostname;
