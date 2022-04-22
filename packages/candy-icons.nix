01f2f9d01a126a88d552fdb61951e19735227967
Krutonium <PFCKrutonium@gmail.com>
2022-04-21 22:23:18 -0400

    Update Icons Stage 1

--- packages/candy-icons.nix
+++ packages/candy-icons.nix
@@ -1,7 +1,7 @@
 { config, pkgs, fetchurl, ... }:
 let
   icon_file = builtins.fetchurl {
-    url = "https://github.com/EliverLara/candy-icons/archive/6503d27bd95d0ad5c1f57ea7a709fe9dd65af547.zip";
+    url = "https://github.com/EliverLara/candy-icons/archive/dfef69fa4f5348372b69378e4caeb02c8b719be7.zip";
     sha256 = "0bal0vz2qr7wsr3m0v2qh1f3hjb7fqxnw4h1dxx5r1fbjy6qm09a";
   };
 
