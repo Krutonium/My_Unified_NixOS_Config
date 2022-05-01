4653a5fd1856df7934099cfe5417ed6e24e3f74b
Krutonium <PFCKrutonium@gmail.com>
2022-05-01 13:35:21 -0400

    Timeout 1

--- tweaks/VL805.nix
+++ tweaks/VL805.nix
@@ -3,7 +3,7 @@
   # My PCIe Expansion card which features a VL805 crashes if native iommu groups are used.
   # Software IOMMU fixes this.
 
-  boot.kernelParams = [ "iommu=soft" "udev.event-timeout=5" ];
+  boot.kernelParams = [ "iommu=soft" "udev.event-timeout=1" ];
 
   # Update: Just fucking disable it
   #boot.kernelModules = ["vfio-pci"];
