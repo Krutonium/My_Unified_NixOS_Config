428a5d503aa56bcf6da2ed2d79483c8be372bd86
Krutonium <PFCKrutonium@gmail.com>
2022-05-01 13:20:04 -0400

    fastboot

--- tweaks/VL805.nix
+++ tweaks/VL805.nix
@@ -3,7 +3,7 @@
   # My PCIe Expansion card which features a VL805 crashes if native iommu groups are used.
   # Software IOMMU fixes this.
 
-  boot.kernelParams = [ "iommu=soft" ];
+  boot.kernelParams = [ "iommu=soft" "fastboot" ];
 
   # Update: Just fucking disable it
   #boot.kernelModules = ["vfio-pci"];
