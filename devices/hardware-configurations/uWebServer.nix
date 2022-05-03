856dbb8547735b6fa8b1cf77a3a9b6926e0ad5f9
Krutonium <PFCKrutonium@gmail.com>
2022-05-03 19:23:47 -0400

    Mount transmission stuff

--- devices/hardware-configurations/uWebServer.nix
+++ devices/hardware-configurations/uWebServer.nix
@@ -100,6 +100,13 @@
       fsType = "btrfs";
       options = [ "subvol=root" ];
     };
+
+  fileSystems."/var/lib/transmission" = 
+    { device = "/dev/disk/by-uuid/a018b12f-6567-4edb-8026-be9292738b4d";
+      fsType = "btrfs";
+      options = [ "subvol=transmission-db" ];
+    };
+
   
   fileSystems."/media" = 
     { device = "/dev/disk/by-id/ata-HGST_HDN726060ALE614_K1G6YP2B-part3";
