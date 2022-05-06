2f6d3925f216a6a5915e91f31507573397c0fde1
Krutonium <PFCKrutonium@gmail.com>
2022-05-06 00:29:49 -0400

    Apparently I need this now

--- common.nix
+++ common.nix
@@ -56,6 +56,9 @@ in
     pkgs.nix-index
   ];
 
+  nixpkgs.config.permittedInsecurePackages = ["nodejs-12.22.12"];
+
+
   programs.gnupg.agent = {
     enable = true;
     pinentryFlavor = "gnome3";
