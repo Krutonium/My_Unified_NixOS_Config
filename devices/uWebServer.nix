fe8a66ee6dfd2f71209651ec06f2ffbd6d0452d4
Krutonium <PFCKrutonium@gmail.com>
2022-05-22 05:34:58 -0400

    Fix IPV6

--- devices/uWebServer.nix
+++ devices/uWebServer.nix
@@ -10,6 +10,7 @@ in
   networking.interfaces."enp3s0".ipv6.addresses = [{ address = "2607:fea8:7a43:7600::523"; prefixLength = 64; }];
   networking.interfaces."enp3s0".ipv4.addresses = [{ address = "192.168.0.10"; prefixLength = 24; }];
   networking.defaultGateway = { address = "192.168.0.1"; interface = "enp3s0"; };
+  networking.defaultGateway6 = { address = "fe80::9050:caff:fe21:cec1"; interface = "enp3s0"; };
   networking.firewall.allowedTCPPorts = [ 25565 26666 ]; #Somtimes Minecraft Servers are run without being explicitly enabled for testing purposes.
   imports =
     [
