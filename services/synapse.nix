10c1ee3ae880b65f0b52f8ae549e0bd77267f161
Krutonium <PFCKrutonium@gmail.com>
2022-05-28 02:44:58 -0400

    Add TURN server

--- services/synapse.nix
+++ services/synapse.nix
@@ -1,32 +1,48 @@
 { config, pkgs, ... }:
+let
+ # Load Coturn Key from /persist/coturn
+
+in
 {
-  services.matrix-synapse = {
-    enable = true;
-    dataDir = "/persist/matrix-data";
-    settings.database_name = "psycopg2";
-    settings.server_name = "krutonium.ca";
-    settings.enable_registration = false;
-    settings.max_upload_size = "50M";
-    settings.listeners = [{
-      port = 8008;
-      bind_addresses = ["127.0.0.1"]; # only local, handled by nginx reverse-proxy
-      type = "http";
-      tls = false;
-      x_forwarded = true;
-      resources = [{
-        names = [ "client" "federation" ];
-        compress = false;
-      }];
-    }];
-  };
-  services.postgresql = {
-    enable = true;
-    initialScript = pkgs.writeText "synapse-init.sql" ''
-      CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
-      CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
-      TEMPLATE template0
-      LC_COLLATE = "C"
-      LC_CTYPE = "C";
-    '';
+  services = {
+    matrix-synapse = {
+      enable = true;
+      dataDir = "/persist/matrix-data";
+      settings = {
+        database_name = "psycopg2";
+        server_name = "krutonium.ca";
+        enable_registration = false;
+        max_upload_size = "50M";
+        turn_uris = [
+          "turn:staticauth.openrelay.metered.ca:80"
+          "turn:staticauth.openrelay.metered.ca:443"
+          "turn:staticauth.openrelay.metered.ca:80?transport=tcp"
+          "turn:staticauth.openrelay.metered.ca:443?transport=tcp"
+          "turns:staticauth.openrelay.metered.ca:443"
+        ];
+        turn_shared_secret = "openrelayprojectsecret";
+        listeners = [{
+          port = 8008;
+          bind_addresses = ["127.0.0.1"]; # only local, handled by nginx reverse-proxy
+          type = "http";
+          tls = false;
+          x_forwarded = true;
+          resources = [{
+            names = [ "client" "federation" ];
+            compress = false;
+          }];
+        }];
+      };
+    };
+    postgresql = {
+      enable = true;
+      initialScript = pkgs.writeText "synapse-init.sql" ''
+        CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
+        CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+        TEMPLATE template0
+        LC_COLLATE = "C"
+        LC_CTYPE = "C";
+      '';
+    };
   };
 }
