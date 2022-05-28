f1a3cf0fd09649a95086469f70003779cb6cdad5
Krutonium <PFCKrutonium@gmail.com>
2022-05-27 23:47:38 -0400

    Refactor KDE

--- desktops/kde.nix
+++ desktops/kde.nix
@@ -1,20 +1,59 @@
 { config, pkgs, ... }:
 {
-  services.xserver.enable = true;
-  hardware.nvidia.modesetting.enable = false;
-  programs.xwayland.enable = true;
-  #services.xserver.displayManager.sddm.wayland = true;
-  #services.xserver.displayManager.sddm.nvidiaWayland = true;
-  hardware.opengl.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        sddm = {
+          enable = true;
+        };
+      };
+      desktopManager = {
+        desktopManager = {
+          plasma5 = {
+            enable = true;
+          };
+        };
+      };
+    };
   };
+  hardware = {
+    opengl = {
+      enable = true;
+    };
+    nvidia = {
+      modesetting = {
+        enable = true;
+      };
+    };
+    pulseaudio = {
+      enable = false;
+    };
+  };
+  programs = {
+    xwayland = {
+      enable = true;
+    };
+  };
+  security = {
+    rtkit = {
+      enable = true;
+    };
+  };
+
+  services = {
+    pipewire = {
+      enable = true;
+      alse = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse = {
+        enable = true;
+      };
+      jack = {
+        enable = true;
+      };
+    };
+  };1
 }
