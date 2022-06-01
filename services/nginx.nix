6788ba20bc3a9f53a59695c6f5f14ef9afeaa3e3
Krutonium <PFCKrutonium@gmail.com>
2022-06-01 03:44:44 -0400

    jellyfin options

--- services/nginx.nix
+++ services/nginx.nix
@@ -142,6 +142,19 @@
       forceSSL = true;
       enableACME = true;
       locations."/".proxyPass = "http://127.0.0.1:8096";
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $http_host;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_pass_request_headers on;
+        proxy_set_header Connection $http_connection;
+        proxy_buffering off;
+        proxy_http_version 1.1;
+        proxy_redirect off;
+      '';
     };
   };
 }
