d068f3f009690f7a07a0a17b2412102713aac578
Krutonium <PFCKrutonium@gmail.com>
2022-06-05 04:31:01 -0400

    Enable nvidia offload

--- devices/uMsiLaptop.nix
+++ devices/uMsiLaptop.nix
@@ -2,6 +2,13 @@
 let
   kernel = pkgs.linuxPackages_zen;
   Hostname = "uMsiLaptop";
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
 in
 {
   # Set our Kernel
@@ -37,8 +44,17 @@ in
   #  system = "x86_64-linux";
   #};
 
-  #services.onedrive.enable = true;
-  #hardware.bumblebee.enable = true;
+  environment.systemPackages = [ nvidia-offload ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    nvidiaBusId = "PCI:1:0:0";
+    intelBusId = "PCI:0:2:0";
+  };
+
   home-manager.users.krutonium = import ../home-manager-files/krutonium/home.nix;
   home-manager.users.root = import ../home-manager-files/root/root.nix;
   networking.hostName = Hostname;
