1f3590a130621b49274335ebb282a093e93ce738
Krutonium <PFCKrutonium@gmail.com>
2022-06-06 03:07:36 -0400

    deploy

--- packages/remote-builders.nix
+++ packages/remote-builders.nix
@@ -23,7 +23,7 @@
       #uMsiLaptop
       hostName = "192.168.0.20";
       system = "x86_64-linux";
-      maxJobs = 4;
+      maxJobs = 8;
       speedFactor = 1;
       supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
       mandatoryFeatures = [];
