2f10f0e80a0b520c880204c7b6fdc555bce5ef37
Krutonium <PFCKrutonium@gmail.com>
2022-06-06 02:26:06 -0400

    Use nix built in distributed building

---
+++ packages/remote-builders.nix
@@ -0,0 +1,32 @@
+{ pkgs, config, ... }:
+{
+  nix.buildMachines = [
+    {
+      #uWebServer
+      hostName = "192.168.0.10";
+      system = "x86_64-linux";
+      maxJobs = 1;
+      speedFactor = 2;
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [];
+    }
+    {
+      #uGamingPC
+      hostName = "192.168.0.40";
+      system = "x86_64-linux";
+      maxJobs = 1;
+      speedFactor = 2;
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [];
+    }
+    {
+      #uMsiLaptop
+      hostName = "192.168.0.20";
+      system = "x86_64-linux";
+      maxJobs = 1;
+      speedFactor = 1;
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [];
+    }
+  ];
+}
