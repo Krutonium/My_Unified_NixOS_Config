2f7a1684de6672f9fa25536745b992bf791f1c04
Krutonium <PFCKrutonium@gmail.com>
2022-06-07 03:25:44 -0400

    Enable NFS

--- services/samba.nix
+++ services/samba.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  networking.firewall.allowedTCPPorts = [ 445 139 ];
+  networking.firewall.allowedTCPPorts = [ 445 139 2049];
   networking.firewall.allowedUDPPorts = [ 137 138 ];
   services.samba = {
     enable = true;
@@ -22,5 +22,11 @@
         "read only" = "no";
       };
     };
+  };
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /home/krutonium/share 192.168.0.10(rw,nohide,insecure,no_subtree_check)
+    '';
   };
 }
