0b75a913b5f83bbf9b9b55787332cfcf6c45c1c4
Krutonium <PFCKrutonium@gmail.com>
2022-06-09 09:36:43 -0400

    Badlion with Icon!

--- packages/badlion.nix
+++ packages/badlion.nix
@@ -1,16 +1,24 @@
-{ pkgs, config, fetchurl, appimageTools, ...}:
+{ pkgs, config, fetchurl, appimageTools, makeDesktopItem, ...}:
 let
   pkgSrc = builtins.fetchurl {
     url = "https://client-updates-cdn77.badlion.net/BadlionClient";
     sha256 = "0ixngc740nxdlqhkykfqyk6b5ypmk0bhp601z5ycnf5xp9mrmccf";
   };
-   BadClient = pkgs.appimageTools.wrapType2 { # or wrapType1
+  BadlionClient = pkgs.appimageTools.wrapType2 { # or wrapType1
     name = "BadlionClient";
     src = pkgSrc;
     extraPkgs = pkgs: with pkgs; [ pkgs.jdk8 pkgs.jdk ];
   };
+  BadlionClientIcon = pkgs.makeDesktopItem {
+    name = "Badlion Client";
+    exec = "BadlionClient";
+    icon = "minecraft";
+    comment = "A Minecraft Client with Anticheat!";
+    desktopName = "Badlion Client";
+    genericName = "Badlion Client";
+    categories = [ "X-games" ];
+  };
 in
 {
-  environment.systemPackages = [ BadClient ];
-  # TODO: Desktop Icon
+  environment.systemPackages = [ BadlionClient BadlionClientIcon ];
 }
