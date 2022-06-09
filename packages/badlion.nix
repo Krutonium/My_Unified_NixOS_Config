12546a27934f7aa7b947896188244af847253649
Krutonium <PFCKrutonium@gmail.com>
2022-06-09 08:53:40 -0400

    Cleanup & Todo

--- packages/badlion.nix
+++ packages/badlion.nix
@@ -4,12 +4,13 @@ let
     url = "https://client-updates-cdn77.badlion.net/BadlionClient";
     sha256 = "0ixngc740nxdlqhkykfqyk6b5ypmk0bhp601z5ycnf5xp9mrmccf";
   };
-   application = pkgs.appimageTools.wrapType2 { # or wrapType1
+   BadClient = pkgs.appimageTools.wrapType2 { # or wrapType1
     name = "BadlionClient";
     src = pkgSrc;
     extraPkgs = pkgs: with pkgs; [ pkgs.jdk8 pkgs.jdk ];
   };
 in
 {
-  environment.systemPackages = [ application ];
+  environment.systemPackages = [ BadClient ];
+  # TODO: Desktop Icon
 }
