239af76837e0777cef806227d6e3ecdc06e9d76a
Krutonium <PFCKrutonium@gmail.com>
2022-06-15 11:07:58 -0400

    Swap to pulling ssh key from GitHub

--- users/krutonium.nix
+++ users/krutonium.nix
@@ -1,12 +1,14 @@
 { config, pkgs, ... }:
+let
+  fetchKeys = username:
+    (builtins.fetchurl "https://github.com/${username}.keys");
+in
 {
   users.users.krutonium = {
     isNormalUser = true;
     shell = pkgs.fish;
     extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBGydZMghVpYF+glHje55hN0/00i9nOEA+OP4A/eneXp krutonium@uHPLaptop - Wed, Mar 16, 2022 @ 4:03 AM"
-    ];
+    openssh.authorizedKeys.keyFiles = [ (fetchKeys "krutonium" ) ];
     hashedPassword = "$6$l5HeZlsZILfJPHoJ$bE95YsS6Xu1kTj9RgPKpd4JblUsoA35UmCrqFdr5N71HNa3T3SA3Nw.RxT4ifqF239DzYECcyZQZQGLCtFb8W/";
   };
   users.extraGroups.vboxusers.members = [ "krutonium" ];
