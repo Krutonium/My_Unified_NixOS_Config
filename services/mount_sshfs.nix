629b3b624d9a28ca7052fd47d0984d25fea99fc3
Krutonium <PFCKrutonium@gmail.com>
2022-06-24 23:05:25 -0400

    Swap to using external so that it works remotely

--- services/mount_sshfs.nix
+++ services/mount_sshfs.nix
@@ -2,7 +2,7 @@
 {
   fileSystems."/home/krutonium/share" =
     {
-      device = "192.168.0.10:/home/krutonium/share";
+      device = "krutonium.ca:/home/krutonium/share";
       fsType = "sshfs";
       options =  [ "allow_other" "IdentityFile=/home/krutonium/.ssh/id_ed25519" ];
     };
