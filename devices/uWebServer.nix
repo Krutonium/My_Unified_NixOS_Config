87c822a55eccdfc3ab53bd7c3389ca21b6b580fd
Krutonium <PFCKrutonium@gmail.com>
2022-07-18 08:00:05 -0400

    deploy

--- devices/uWebServer.nix
+++ devices/uWebServer.nix
@@ -7,10 +7,10 @@ in
   # Set our Kernel
   boot.kernelPackages = kernel;
   networking.hostName = "uWebServer";
-  #networking.interfaces."enp3s0".ipv6.addresses = [{ address = "2607:fea8:7a43:7600::523"; prefixLength = 64; }];
+  networking.interfaces."enp3s0".ipv6.addresses = [{ address = "2607:fea8:7a43:7600:428d:5cff:fe54:8996"; prefixLength = 64; }];
   networking.interfaces."enp3s0".ipv4.addresses = [{ address = "192.168.0.10"; prefixLength = 24; }];
   networking.defaultGateway = { address = "192.168.0.1"; interface = "enp3s0"; };
-  #networking.defaultGateway6 = { address = "fe80::9050:caff:fe21:cec1"; interface = "enp3s0"; };
+  networking.defaultGateway6 = { address = "fe80::1"; interface = "enp3s0"; };
   networking.firewall.allowedTCPPorts = [ 25565 26666 ]; #Somtimes Minecraft Servers are run without being explicitly enabled for testing purposes.
   imports =
     [
