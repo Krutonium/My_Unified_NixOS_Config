e0b3a9e9f5b49c568464aef37ae8a80eda966201
Krutonium <PFCKrutonium@gmail.com>
2022-08-02 15:36:29 -0400

    Disable GCC stuff

--- users/krutonium/packages.nix
+++ users/krutonium/packages.nix
@@ -57,13 +57,13 @@ in
       pkgs.qmk
       pkgs.qmk-udev-rules
       pkgs.arduino
-      pkgs.gcc11
-      pkgs.pkgsCross.avr.buildPackages.gcc
-      pkgs.avrdude
-      pkgs.gcc-arm-embedded
-      pkgs.gnumake
+      #pkgs.gcc11
+      #pkgs.pkgsCross.avr.buildPackages.gcc
+      #pkgs.avrdude
+      #pkgs.gcc-arm-embedded
+      #pkgs.gnumake
       pkgs.git
-      pkgs.python39Full
+      #pkgs.python39Full
 
       # Media
       pkgs.vlc
