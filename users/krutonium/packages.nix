70b37cce98270f97ed587e73171c8fbef30f1bec
Krutonium <PFCKrutonium@gmail.com>
2022-08-02 15:09:29 -0400

    Mis-spelled idea-ultimate

--- users/krutonium/packages.nix
+++ users/krutonium/packages.nix
@@ -13,7 +13,7 @@ in
       ''
         ${pkgs.steam-run}/bin/steam-run ${pkgs.jetbrains.idea-ultimate}/bin/idea-ultimate
       '';
-      idea = pkgs.jetbrains.idea-uiltimate.overrideAttrs (oldAttrs: { meta.priority = 10; });
+      idea = pkgs.jetbrains.idea-ultimate.overrideAttrs (oldAttrs: { meta.priority = 10; });
       riderScript = pkgs.writeShellScriptBin "rider"
       ''
         ${pkgs.steam-run}/bin/steam-run ${pkgs.jetbrains.rider}/bin/rider
