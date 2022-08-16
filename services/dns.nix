c03e4b742ecd86e0ac410d74e40e4a99c3709777
Krutonium <PFCKrutonium@gmail.com>
2022-08-15 23:46:53 -0400

    deploy

--- services/dns.nix
+++ services/dns.nix
@@ -4,16 +4,10 @@ services.coredns.enable = true;
 services.coredns.config =
   ''
     . {
-      # Cloudflare and Google
-      forward . 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4
-      cache
-    }
-    hosts {
-       192.168.0.10 uwebserver.local
-       192.168.0.20 umsilaptop.local
-       192.168.0.40 ugamingpc.local
-       fallthrough
-    }
+        # Cloudflare and Google
+        forward . 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4
+        cache
+      }
     }
   '';
 networking.firewall.allowedTCPPorts = [ 53 ];
