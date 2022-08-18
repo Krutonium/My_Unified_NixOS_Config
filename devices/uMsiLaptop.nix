05240e7e338bdbdd542d722910d7cfccc3557651
Krutonium <PFCKrutonium@gmail.com>
2022-08-18 18:43:17 -0400

    deploy

--- devices/uMsiLaptop.nix
+++ devices/uMsiLaptop.nix
@@ -60,7 +60,7 @@ in
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.prime = {
-    offload.enable = true;
+    #offload.enable = true;
     nvidiaBusId = "PCI:1:0:0";
     intelBusId = "PCI:0:2:0";
     sync.enable = true;
