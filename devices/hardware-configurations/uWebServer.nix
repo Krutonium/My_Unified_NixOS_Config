1c9aa257e23a1e75c9e74e5fbfa6d1a89f9debea
Krutonium <PFCKrutonium@gmail.com>
2022-08-28 12:32:37 -0400

    deploy

--- devices/hardware-configurations/uWebServer.nix
+++ devices/hardware-configurations/uWebServer.nix
@@ -124,7 +124,7 @@
       options = [ "subvol=jellyfin" ];
     };
 
-  fileSystems."/var/www/" =
+  fileSystems."/var/www" =
     { device = "/dev/disk/by-uuid/a018b12f-6567-4edb-8026-be9292738b4d";
       fsType = "btrfs";
       options = [ "subvol=www" ];
