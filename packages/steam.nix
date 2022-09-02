01970b69cf5e728db42e32404e64e8ac623f9557
Krutonium <PFCKrutonium@gmail.com>
2022-09-02 09:45:53 -0400

    MangoHud and Gamescope

--- packages/steam.nix
+++ packages/steam.nix
@@ -1,21 +1,22 @@
 { config, pkgs, ... }:
-let
-  steamScript = pkgs.writeShellScriptBin "steam"
-    ''
-      mangohud --dlsym ${pkgs.steam}/bin/steam
-    '';
-  lowSteam = pkgs.steam.overrideAttrs (oldAttrs: { meta.priority = 10; });
-
-  #What have we learned today? Basically, this doesn't work. It ends up making Steam either not start at all, or start with a blank screen.
-in
 {
 
   # Gotta have good old steam
   programs.steam.enable = true;
   hardware.steam-hardware.enable = true;
-  environment.systemPackages = [
-    #steamScript
-    lowSteam
-  ];
-
+  nixpkgs.config.packageOverrides = pkgs: {
+    extraPkgs = pkgs: with pkgs; [
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXScrnSaver
+      libpng
+      libpulseaudio
+      libvorbis
+      stdenv.cc.cc.lib
+      libkrb5
+      keyutils
+    ];
+  };
+  environment.systemPackages = [ pkgs.gamescope pkgs.mangohud ];
 }
