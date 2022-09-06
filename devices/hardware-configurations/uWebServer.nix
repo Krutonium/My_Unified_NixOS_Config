f6fb8b9638dcdc8d49cab083ec2ec9a525368de5
Krutonium <PFCKrutonium@gmail.com>
2022-09-06 14:07:00 -0400

    Softether

--- devices/hardware-configurations/uWebServer.nix
+++ devices/hardware-configurations/uWebServer.nix
@@ -129,6 +129,12 @@
       fsType = "btrfs";
       options = [ "subvol=www" ];
     };
+    
+  fileSystems."/var/lib/softether" =
+     { device = "/dev/disk/by-uuid/a018b12f-6567-4edb-8026-be9292738b4d";
+       fsType = "btrfs";
+       options = [ "subvol=softether" ];
+     };
 
   swapDevices = [ ];
 
