6d75f2fe369aaa358ae764d5bfeacb725dda4f1d
Krutonium <PFCKrutonium@gmail.com>
2022-09-06 14:16:15 -0400

    Open Port

--- services/SoftetherVPN.nix
+++ services/SoftetherVPN.nix
@@ -1,9 +1,11 @@
 { pkgs, config, ...}:
 {
+ networking.firewall.allowedTCPPorts = [ 5555 ];
  # SoftetherVPN Server, but Clients will use built in VPN solutions I think
  services.softether = {
    enable = true;
    vpnserver.enable = true;
    dataDir = "/var/lib/softether";
  };
+
 }
