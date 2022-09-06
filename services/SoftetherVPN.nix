e81d00fc56c5c47e7240fd8b6f81c455513e301d
Krutonium <PFCKrutonium@gmail.com>
2022-09-06 14:13:12 -0400

    Enable

--- services/SoftetherVPN.nix
+++ services/SoftetherVPN.nix
@@ -2,6 +2,7 @@
 {
  # SoftetherVPN Server, but Clients will use built in VPN solutions I think
  services.softether = {
+   enable = true;
    vpnserver.enable = true;
    dataDir = "/var/lib/softether";
  };
