74350738f1078f2f8511b8f4fd9bead8419c80ce
Krutonium <PFCKrutonium@gmail.com>
2022-09-07 16:45:21 -0400

    Chicago95

--- packages/plymouth.nix
+++ packages/plymouth.nix
@@ -18,6 +18,27 @@ let
       # Apparently using cat and sed breaks? I don't know why. So we summon an eldritch force to do it for us.
     '';
   };
+
+  # Windows 95 Boot Screen
+  win95_files = builtins.fetchurl {
+    url = "https://github.com/grassmunk/Chicago95/archive/refs/heads/master.zip";
+    sha256 = "";
+  };
+
+    win95-plymouth = pkgs.stdenv.mkDerivation {
+        name = "win95-plymouth";
+        buildInputs = [ pkgs.unzip pkgs.moreutils ];
+        src = win95_files;
+        buildCommand = ''
+        outDir="$out/share/plymouth/themes/"
+        mkdir -p $outDir
+        mkdir -p $outDir/tmp
+        unzip $src -d $outDir/tmp/
+        mv $outDir/tmp/Plymouth/Chicago95 $outDir/Chicago95
+        rm -rd $outDir/tmp
+        sed "s%/usr/share/plymouth/themes/%$outDir%g" $outDir/Chicago95/Chicago95.plymouth | sponge $outDir/Chicago95/Chicago95.plymouth
+        '';
+    };
 in
 {
   environment.systemPackages = [
@@ -25,8 +46,10 @@ in
   ];
 
   boot.plymouth.enable = true;
-  boot.plymouth.theme = "aperture";
+  #boot.plymouth.theme = "aperture";
+  boot.plymouth.theme = "Chicago95";
   boot.plymouth.themePackages = [
     aperture-plymouth
+    win95-plymouth
   ];
 }
