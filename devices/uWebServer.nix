3ea897327854ec7f6095acfa4d7b9a774a0c8eaa
Krutonium <PFCKrutonium@gmail.com>
2022-10-01 01:35:09 -0400

    Enable Nextcloud

--- devices/uWebServer.nix
+++ devices/uWebServer.nix
@@ -29,7 +29,7 @@ in
       #../services/satisfactory_server.nix
       #../services/project_ozone_3.nix
       ../services/create.nix
-      #../services/nextcloud.nix
+      ../services/nextcloud.nix
       #../services/seafile.nix
       ../users/krutonium/user_server.nix
       ../users/resin/user.nix
