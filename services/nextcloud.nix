61c6c5492dff587b212661ba990159ffa338e344
Krutonium <PFCKrutonium@gmail.com>
2022-10-02 00:39:48 -0400

    deploy

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -5,7 +5,7 @@
     https = true;
     appstoreEnable = true;
     enableImagemagick = true;
-    maxUploadSize = "10240M"
+    maxUploadSize = "10240M";
     package = pkgs.nextcloud24;
     hostName = "nextcloud.krutonium.ca";
     config.adminpassFile = "/persist/nextcloud-admin-pass";
